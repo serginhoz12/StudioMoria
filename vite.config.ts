ec9d6f12c274342dd1644b06989c983760becb31
Sergio Madeira <serginhoz12@gmail.com>
2026-03-06 23:44:48 -0300

    remove PWA e corrigir build

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,33 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import fs from 'node:fs';
 
-const manifest = JSON.parse(fs.readFileSync('./manifest.json', 'utf-8'));
+export default defineConfig(({ mode }: { mode: string }) => {
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [
-        react(),
-        VitePWA({
-          registerType: 'autoUpdate',
-          includeAssets: ['icon-192.png'],
-          manifest: manifest,
-          workbox: {
-            globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-          }
-        })
-      ],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    plugins: [
+      react()
+    ],
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+
+  };
+
 });
